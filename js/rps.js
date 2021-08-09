5e51d3287fd0684d47dc31c3302f7e8e4177efb3
jochuu <reyn@live.co.uk>
2021-08-09 15:07:48 +0100

    Add scores to show on page, game finishes at 5 and next player selection auto resets scores

--- js/rps.js
+++ js/rps.js
@@ -1,37 +1,58 @@
-selection = ['rock', 'paper', 'scissors']
-computerScore = 0;
-playerScore = 0;
+let selection = ['rock', 'paper', 'scissors'];
+let computerScore = 0;
+let playerScore = 0;
 
 function computerSelection() {
     return selection[Math.floor(Math.random()*selection.length)];
 }
 
-function playerSelection(input) {
-    playRound(computerSelection(), input);
-    console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`);
+function playerSelection(input) { //input is value passed from button's onclick
+    if(computerScore === 5 || playerScore === 5 ) {
+        resetGame();
+        playRound(computerSelection(), input);
+    }
+     else {
+         playRound(computerSelection(), input);
+        }
 }
 
 function playRound(computer, player) {
-    console.log('Computer chose:' + computer, 'Player chose:' + player);
-    if (computer == player) {
-        console.log("It's a tie");
-    } else if (
-        (computer == "rock" && player == "scissors") ||
-        (computer == "paper" && player == "rock") ||
-        (computer == "scissors" && player == "paper")
-        ) {
-        console.log("Computer Wins!");
-        computerScore++;
-        } else {
-            console.log("Player Wins!");
-            playerScore++;
-        }
+    document.querySelector('#currentRound').textContent = `Computer chose: ${upperFirstLetter(computer)},  Player chose: ${upperFirstLetter(player)}`;
+        let outcome = '';
+        if (computer === player) {
+            outcome = "It's a tie!"
+        } else if (
+            (computer === "rock" && player === "scissors") ||
+            (computer === "paper" && player === "rock") ||
+            (computer === "scissors" && player === "paper")
+            ) {
+                computerScore++;
+                outcome = "Computer Wins!"
+            } else {
+                playerScore++;
+                outcome = "Player Wins!"
+            }
+    updatePage(computerScore, playerScore, outcome);
+}
+
+function updatePage(computer, player, outcome) {
+
+    document.querySelector('#computerScore').textContent = computer;
+    document.querySelector('#playerScore').textContent = player;
+
+    if (computerScore === 5 || playerScore === 5) {
+        document.querySelector('#outcome').textContent = 'Game Over!';
+
+    } else {
+        document.querySelector('#outcome').textContent = outcome;
     }
+}
 
+function upperFirstLetter(text) {
+    return text[0].toUpperCase() + text.substring(1);
+}
 
-// (function() {
-//     for (let i = 0; i < 5; i++) {
-//     playRound(computerSelection(), playerSelection());
-// }
-// })();
-
+function resetGame() {
+    computerScore = 0;
+    playerScore = 0;
+}
